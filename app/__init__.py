431bf57fcc189ba458a5310e011519eba0e31295
KJ <irateboolean@gmx.com>
2021-05-26 11:12:06 -0400

    added bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from config import Config
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from logging.handlers import RotatingFileHandler
 from logging.handlers import SMTPHandler
 import logging
 import os
-from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +17,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap=Bootstrap(app)
+
 if not app.debug or True:
     if app.config['MAIL_SERVER']:
         auth = None
